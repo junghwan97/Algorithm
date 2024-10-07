f6bf664fdf7335b3a3d87cf22eb67385cfc4fc3c
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2024-10-07 09:59:31 +0900

    [Bronze III] Title: 곱셈, Time: 100 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,19 +1,31 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt();
-		int b = sc.nextInt();
+        public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		System.out.println(a*(b%10));
-		System.out.println(a*(int)(Math.floor((b%100)/10)));
-		System.out.println(a*(b/100));	
-		System.out.println(a*b);	
+            int A = Integer.parseInt(br.readLine());
+            int B = Integer.parseInt(br.readLine());
 
-	
-	}
+            br.close();
 
+            StringBuilder sb = new StringBuilder();
+
+            sb.append(A * (B%10));
+            sb.append('\n');
+
+            sb.append(A * ((B%100)/10));
+            sb.append('\n');
+
+            sb.append(A * (B/100));
+            sb.append('\n');
+
+            sb.append(A * B);
+
+            System.out.print(sb);
+
+        }
 }
