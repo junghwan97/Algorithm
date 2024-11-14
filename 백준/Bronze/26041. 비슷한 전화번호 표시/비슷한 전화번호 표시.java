4c4731623fe1a5ac935ae3edf2007aa868ce1a24
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2024-11-14 22:21:05 +0900

    [Bronze II] Title: 비슷한 전화번호 표시, Time: 268 ms, Memory: 27384 KB -BaekjoonHub

---
+++ 백준/Bronze/26041. 비슷한 전화번호 표시/비슷한 전화번호 표시.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        String n = br.readLine();
+        int count = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if (arr[i].startsWith(n) && !arr[i].equals(n)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
