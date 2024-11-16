0e0a91acd293336fc4fa93e814d030891edfda61
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2024-11-16 10:27:17 +0900

    [Bronze II] Title: 모스 부호, Time: 104 ms, Memory: 14104 KB -BaekjoonHub

---
+++ 백준/Bronze/29701. 모스 부호/모스 부호.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static Map<String, Character> morseMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        initMorse();
+
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            sb.append(morseMap.get(arr[i]));
+        }
+        System.out.println(sb);
+    }
+
+    private static void initMorse() {
+        morseMap.put(".-", 'A');
+        morseMap.put("-...", 'B');
+        morseMap.put("-.-.", 'C');
+        morseMap.put("-..", 'D');
+        morseMap.put(".", 'E');
+        morseMap.put("..-.", 'F');
+        morseMap.put("--.", 'G');
+        morseMap.put("....", 'H');
+        morseMap.put("..", 'I');
+        morseMap.put(".---", 'J');
+        morseMap.put("-.-", 'K');
+        morseMap.put(".-..", 'L');
+        morseMap.put("--", 'M');
+        morseMap.put("-.", 'N');
+        morseMap.put("---", 'O');
+        morseMap.put(".--.", 'P');
+        morseMap.put("--.-", 'Q');
+        morseMap.put(".-.", 'R');
+        morseMap.put("...", 'S');
+        morseMap.put("-", 'T');
+        morseMap.put("..-", 'U');
+        morseMap.put("...-", 'V');
+        morseMap.put(".--", 'W');
+        morseMap.put("-..-", 'X');
+        morseMap.put("-.--", 'Y');
+        morseMap.put("--..", 'Z');
+        morseMap.put(".----", '1');
+        morseMap.put("..---", '2');
+        morseMap.put("...--", '3');
+        morseMap.put("....-", '4');
+        morseMap.put(".....", '5');
+        morseMap.put("-....", '6');
+        morseMap.put("--...", '7');
+        morseMap.put("---..", '8');
+        morseMap.put("----.", '9');
+        morseMap.put("-----", '0');
+        morseMap.put("--..--", ',');
+        morseMap.put(".-.-.-", '.');
+        morseMap.put("..--..", '?');
+        morseMap.put("---...", ':');
+        morseMap.put("-....-", '-');
+        morseMap.put(".--.-.", '@');
+    }
+}
