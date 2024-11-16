11be40b19c87c6061cd1ecb401c17f0cdcf33a01
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2024-11-16 13:00:59 +0900

    [Silver III] Title: 파일 정리, Time: 1016 ms, Memory: 52600 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -8,19 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         Map<String, Integer> file = new HashMap<>();
-        int sum = 1;
-
+  
         for (int i = 0; i < N; i++) {
             String s = br.readLine();
             int n = s.indexOf(".");
             String extension = s.substring(n + 1, s.length());
-            if (!file.containsKey(extension)) {
-                file.put(extension, sum);
-            } else {
-                sum = file.get(extension) + 1;
-                file.put(extension, sum);
-                sum = 1;
-            }
+            file.put(extension, file.getOrDefault(extension, 0) + 1);
         }
         Set<String> keySet = file.keySet();
         List<String> list = new ArrayList<>(keySet);
@@ -28,7 +21,5 @@ public class Main {
         for (String s : list) {
             System.out.println(s + " " + file.get(s));
         }
-
-
     }
 }
