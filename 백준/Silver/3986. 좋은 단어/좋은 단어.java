d0553a728e4a277c13df4c323a00041b37763137
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2024-11-27 14:26:13 +0900

    [Silver IV] Title: 좋은 단어, Time: 288 ms, Memory: 22028 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.java
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine()); // 단어의 수
@@ -10,19 +9,15 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             String s = br.readLine();
-            Stack<Character> stack = new Stack<>(); // 단어가 들어갈 자료구조
+            Stack<Character> stack = new Stack<>(); // 단어가 들어갈 자료구조(새로운 문자열마다 초기화)
 
             for (int j = 0; j < s.length(); j++) {
-                if (stack.empty()) {
-                    stack.push(s.charAt(j));
-                } else {
-                    char a = s.charAt(j);
-                    if (a == stack.peek()) {
-                        stack.pop();
-                    } else {
-                        stack.push(s.charAt(j));
-                    }
-                }
+                // 현재 문자
+                char a = s.charAt(j);
+                // 스택이 비어있지 않거나 현재 문자가 스택의 최상단과 같을 경우
+                if (!stack.empty() && a == stack.peek()) stack.pop();
+                // 스택이 비어있거나 현재 문자가 스택의 최상단과 다를 경우
+                else stack.push(s.charAt(j));
             }
             // 스택이 비어 있다면 좋은 단어
             if (stack.isEmpty()) {
