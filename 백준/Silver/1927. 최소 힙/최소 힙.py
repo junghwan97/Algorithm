fbcff5c12e9b7fadc8aefcffbbf967f3d3a81121
junghwan97 <123052062+junghwan97@users.noreply.github.com>
2025-03-04 14:39:01 +0900

    [Silver II] Title: 최소 힙, Time: 156 ms, Memory: 123972 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,15 +1,19 @@
 import heapq
 import sys
 
-n = int(sys.stdin.readline())
+# 한 번에 입력 받기
+input = sys.stdin.read
+data = input().split()
+
+n = int(data[0])  # 첫 번째 값이 입력 개수
 min_heap = []
-for i in range(n):
-    num = int(sys.stdin.readline())
+
+# 숫자 처리
+for num in map(int, data[1:]):  # 첫 번째 요소를 제외한 나머지를 숫자로 변환
     if num == 0:
-        if len(min_heap) == 0:
+        if not min_heap:
             print(0)
         else:
-            removed = heapq.heappop(min_heap)
-            print(removed)
+            print(heapq.heappop(min_heap))
     else:
         heapq.heappush(min_heap, num)
